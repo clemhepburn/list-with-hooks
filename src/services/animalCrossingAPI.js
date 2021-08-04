3c8bc958ae1338883edac6643887bc4fbc67fc4f
Clem Hepburn <cooper.hepburn@pcc.edu>
2021-08-04 13:07:27 -0700

    create fetch by id function

--- src/services/animalCrossingAPI.js
+++ src/services/animalCrossingAPI.js
@@ -3,3 +3,10 @@ export const fetchVillagers = async () => {
   const json = await res.json();
   return json;
 };
+
+export const fetchVillagersById = async (id) => {
+  const res = await fetch('https://ac-vill.herokuapp.com/villagers/:id');
+  const json = await res.json();
+  return json;
+};
+
