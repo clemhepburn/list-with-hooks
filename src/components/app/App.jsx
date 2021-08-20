c4b1a9fec57b1f4fe5a925a4b2780eec239cbf3d
Clem Hepburn <clemhepburn@gmail.com>
2021-08-19 18:03:31 -0700

    wire in router

--- src/components/app/App.jsx
+++ src/components/app/App.jsx
@@ -1,7 +1,28 @@
 import React from 'react';
+import {
+  BrowserRouter as Router,
+  Route,
+  Switch,
+  Redirect
+} from 'react-router-dom';
 import Villagers from '../../containers/Villagers';
+import DetailPage from '../../containers/VillagerDetailContainer';
 import './App.css';
 
 export default function App() {
-  return <Villagers />;
+  return <div className="app">
+    <Router>
+      <Switch>
+        <Route exact path="/" render={routerProps => (
+          <Villagers {...routerProps} />
+        )} />
+
+        <Route exact path="/:_id" render={routerProps => (
+          <DetailPage {...routerProps} />
+        )} />
+
+        <Redirect to="/" />
+      </Switch>
+    </Router>
+  </div>
 }
